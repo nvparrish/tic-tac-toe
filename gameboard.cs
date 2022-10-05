0e4a09638343a4a3b5d79137e17942c8262114dc
Nathan V. Parrish <nvparrish@gatech.edu>
2022-10-04 22:36:40 -0400

    Initial commit printing gameboard
    Code that creates the gameboard only.

---
+++ gameboard.cs
@@ -0,0 +1,29 @@
+
+class GameBoard {
+	char [,] grid = new char [,] { { '1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+	public GameBoard() {
+	}
+	public void Display() {
+		System.Console.WriteLine("{0}{1}{2}{3}{4}", grid[0,0], '|', grid[0,1], '|', grid[0,2]);
+		System.Console.WriteLine("-----");
+		System.Console.WriteLine("{0}{1}{2}{3}{4}", grid[1,0], '|', grid[1,1], '|', grid[1,2]);
+		System.Console.WriteLine("-----");
+		System.Console.WriteLine("{0}{1}{2}{3}{4}", grid[2,0], '|', grid[2,1], '|', grid[2,2]);
+	}
+	public void Reset() {
+		grid = new char [,] { { '1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+	}
+	public bool SetSquare(char player, ushort square) {
+		int row = square / 3;
+		int col = square % 3;
+		char core = (char)('1' + 3*row + col);
+		if (core == grid[row,col]) {
+			// Suitable to be replaced
+			grid[row, col] = player;
+			return true;
+		} else {
+			// Unavailable square
+			return false;
+		}
+	}
+}
---
+++ tictactoe.cs
@@ -0,0 +1,8 @@
+
+class TicTacToe {
+	static public void Main(string[] args) {
+		System.Console.WriteLine("Welcome to the Tic Tac Toe game!");
+		GameBoard board = new GameBoard();
+		board.Display();
+	}
+}
